32347220b66cc1a46df0eb430c31ddce0d7609c1
cristian16b <cristian16b>
2021-04-14 23:27:32 -0300

    first commit

---
+++ app/src/main/java/com/example/logingooglefirebase/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.logingooglefirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void logOut(View view) {
+        FirebaseAuth.getInstance().signOut();
+        Intent i = new Intent(this, LoginActivity.class );
+
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+        // Sign Out Google...
+    }
+}
